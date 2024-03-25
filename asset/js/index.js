415ab6221448dd1dfd8333b1cb6728d5b5b1315f
Vinicius Bueno <79877937+Vinicius-a11y@users.noreply.github.com>
2024-03-24 21:25:34 -0300

    obj

---
+++ asset/js/index.js
@@ -0,0 +1,24 @@
+const objs = [
+  {
+    nome: "Vinicius",
+    sobrenome: "Bueno",
+    idade: 22,
+    proficao: "Dev",
+    detalhe_profissao: {
+      profissao: "Programador",
+      empresa: "Ncc Grup",
+    },
+    hobbies: ["Futebol", "Academia"],
+  },
+  {
+    nome: "Luiz",
+    sobrenome: "Neto",
+    idade: 22,
+    proficao: "Dev",
+    detalhe_profissao: {
+      profissao: "Programador",
+      empresa: "Ncc Grup",
+    },
+    hobbies: ["Futebol", "Academia", "Balada"],
+  },
+];
